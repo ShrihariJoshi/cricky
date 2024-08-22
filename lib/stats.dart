691e5399a01faddf808a707ea819f0748caaba44
Shrihari Joshi <joshishrihari3@gmail.com>
2024-08-22 19:01:36 +0530

    .

--- lib/stats.dart
+++ lib/stats.dart
@@ -19,76 +19,78 @@ class Stats extends StatelessWidget {
           ),
         ),
         drawer: Drawer(
-        // backgroundColor: const Color.fromARGB(149, 8, 40, 66),
-        child: ListView(
-          children: <Widget>[
-            const Image(
-              alignment: Alignment.center,
-              colorBlendMode: BlendMode.color,
-              image: AssetImage('assets/images/CRICBASE.jpg'),
-            ),
-            Container(
-              decoration: const BoxDecoration(
-                  border: Border(
-                top: BorderSide(width: 2.0),
-                left: BorderSide(width: 2.0),
-                right: BorderSide(width: 2.0),
-                bottom: BorderSide(width: 1.0),
-              )),
-              child: ListTile(
-                leading: const Icon(Icons.feedback),
-                onTap: () {
-                  Navigator.push(context,
-                      MaterialPageRoute(builder: (context) => const Rating()));
-                },
-                tileColor: const Color.fromARGB(255, 89, 204, 220),
-                title: const Text(
-                  'Rate the App',
-                  style: TextStyle(
-                    fontSize: 18,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.black,
-                  ),
-                ),
+          // backgroundColor: const Color.fromARGB(149, 8, 40, 66),
+          child: ListView(
+            children: <Widget>[
+              const Image(
+                alignment: Alignment.center,
+                colorBlendMode: BlendMode.color,
+                image: AssetImage('assets/images/CRICBASE.jpg'),
               ),
-            ),
-            Container(
-              decoration: const BoxDecoration(
-                border: Border(
-                  top: BorderSide(width: 1.0),
+              Container(
+                decoration: const BoxDecoration(
+                    border: Border(
+                  top: BorderSide(width: 2.0),
                   left: BorderSide(width: 2.0),
                   right: BorderSide(width: 2.0),
-                  bottom: BorderSide(width: 2.0),
+                  bottom: BorderSide(width: 1.0),
+                )),
+                child: ListTile(
+                  leading: const Icon(Icons.feedback),
+                  onTap: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const Rating()));
+                  },
+                  tileColor: const Color.fromARGB(255, 89, 204, 220),
+                  title: const Text(
+                    'Rate the App',
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black,
+                    ),
+                  ),
                 ),
               ),
-              child: ExpansionTile(
-                collapsedBackgroundColor: const Color.fromARGB(255, 89, 204, 220),
-                backgroundColor:
-                    const Color.fromARGB(255, 89, 204, 220),
-                shape: Border.all(
-                  color: Colors.black,
-                  width: 2.0,
-                ),
-                leading: const Icon(Icons.headset_mic, color: Colors.black),
-                title: const Text(
-                  'Support',
-                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-                ),
-                children: const <Widget>[
-                  ListTile(
-                    title: Text('Contact Us: 080-260324XX'),
-                    leading: Icon(Icons.phone, color: Colors.black),
+              Container(
+                decoration: const BoxDecoration(
+                  border: Border(
+                    top: BorderSide(width: 1.0),
+                    left: BorderSide(width: 2.0),
+                    right: BorderSide(width: 2.0),
+                    bottom: BorderSide(width: 2.0),
                   ),
-                  ListTile(
-                    title: Text('Queries: cricbaseapp@gmail.com'),
-                    leading: Icon(Icons.mail, color: Colors.black),
+                ),
+                child: ExpansionTile(
+                  collapsedBackgroundColor:
+                      const Color.fromARGB(255, 89, 204, 220),
+                  backgroundColor: const Color.fromARGB(255, 89, 204, 220),
+                  shape: Border.all(
+                    color: Colors.black,
+                    width: 2.0,
                   ),
-                ],
+                  leading: const Icon(Icons.headset_mic, color: Colors.black),
+                  title: const Text(
+                    'Support',
+                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                  ),
+                  children: const <Widget>[
+                    ListTile(
+                      title: Text('Contact Us: 080-260324XX'),
+                      leading: Icon(Icons.phone, color: Colors.black),
+                    ),
+                    ListTile(
+                      title: Text('Queries: cricbaseapp@gmail.com'),
+                      leading: Icon(Icons.mail, color: Colors.black),
+                    ),
+                  ],
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
-      ),
         body: const Statistics(),
       ),
     );
